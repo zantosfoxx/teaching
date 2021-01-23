55c520b256088a44f0f18a9172e70f736433fb9b
Sylvester Okeleke <S_Okeleke@acs.org>
2021-01-23 13:21:09 -0500

    initial commit

---
+++ Solved/config.py
@@ -0,0 +1,5 @@
+remote_db_endpoint = 'databootcamp.cujqfcf9khsm.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'ZantosFoxx'
+remote_db_name = 'gwsis'
